2b09106de85719ac302ccbf6ce83faf216f12c82
Mahboob <mahboob@Mahboobs-MacBook-Pro.local>
2015-08-26 12:26:32 +0600

    Initial Commit

---
+++ CustomComboBox/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomComboBox
+//
+//  Created by Mahboob on 8/26/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
